13c1ea9a8ef687d66ab70b1a3b28d4ead3652476
Nicholas Hairs <info@nicholashairs.com>
2019-04-28 00:43:22 +1000

    Add git aliases and other helpful aliases

--- zsh/30-aliases.zsh
+++ zsh/30-aliases.zsh
@@ -5,19 +5,26 @@ alias -r lh='ls -lh'
 alias -r ll='ls -l'
 alias -r lah='ls -lha'
 
-alias -r g='/usr/bin/git status'
-alias -r gu='/usr/bin/git pull'
-alias -r gp='/usr/bin/git push'
-alias -r ga='/usr/bin/git add'
-alias -r gd='/usr/bin/git diff'
-alias -r gb='/usr/bin/git branch'
-alias -r gba='/usr/bin/git branch -a'
-alias -r gc='/usr/bin/git commit'
-alias -r gca='/usr/bin/git commit -a'
-alias -r gco='/usr/bin/git checkout'
-alias -r gl="/usr/bin/git log --date-order --graph --pretty=format:'%cred%h%creset -%c(yellow)%d%creset %s %cgreen(%cr) %c(bold blue)<%an>%creset' --abbrev-commit --date=relative"
+alias -r g='git status'
+alias -r gu='git pull'
+alias -r gp='git push'
+alias -r ga='git add'
+alias -r gd='git diff'
+alias -r gb='git branch'
+alias -r gba='git branch -a'
+alias -r gc='git commit'
+alias -r gca='git commit -a'
+alias -r gco='git checkout'
+alias -r gl="git log --date-order --graph --pretty=format:'%C(red)%h%C(reset) -%C(yellow)%d%C(reset) %s %C(green)(%cr) %C(bold blue)<%an>%C(reset)' --abbrev-commit --date=relative"
+alias -r gla="gl --all"
+
+alias -r vim="vim -p"
+
+alias -r nose="nosetests --with-yanc --with-watcher"
+
+alias -r t='tree -I "venv|__pycache__"'
 
 
-alias -r vim="vim -O"
-
-alias nose="nosetests --with-yanc --with-watcher"
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias -r alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
