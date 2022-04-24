3f3d78aa5cb7ece7944c704cc5b037246648a96b
Nicholas Hairs <info@nicholashairs.com>
2022-04-24 14:48:17 +1000

    [host-nhairs-radon] streamline screenlayout

---
+++ host-radon/screenlayout/default.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+xrandr \
+    --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+    --output DP-1-1 --off \
+    --output DP-1-2 --off \
+    --output HDMI-2 --off \
+    --output HDMI-1 --off \
+    --output DP-1 --off \
+    --output DP-1-3 --off \
+    --output DP-2 --off
+
+nitrogen --restore
